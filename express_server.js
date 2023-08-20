b3bd70eddaa24cb460b34da910cc21959e46cf3c
james <jameswu000@gmail.com>
2023-08-19 20:12:28 -0600

    recent express get requests for show_urls page and header partials on index and urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 8080
 
+app.set('view engine', 'ejs')
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +13,29 @@ const urlDatabase = {
 app.get('/', (req, res) => {
   res.send('hello')
 })
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase)
+})
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
+})
+
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars)
+})
+
+app.get("/urls/:id", (req, res) => {
+  
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
+  }
+  res.render("urls_show", templateVars)
+ 
+});
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}`)
